38c146a879afa573fc20e03351a99ad454b2a5a1
sharath reddy <sharath111reddy@gmail.com>
2025-01-08 11:42:29 +0530

    styled signup and signin page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,13 @@
 import Todolist from "./components/todolist"
 import { Route , Routes } from "react-router-dom"
 import Signin from "./components/signin"
+import Signup from "./components/signup"
 
 export default function App(){
   return <div className="">
       <Routes>
-        <Route path="/" element={<Signin></Signin>}></Route>
+        <Route path="/signin" element={<Signin></Signin>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route path="/home" element={<Todolist></Todolist>}></Route>
       </Routes>
   </div>
--- front-end/src/components/signin.jsx
+++ front-end/src/components/signin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,Link } from "react-router-dom";
 export default function Signin(){
         const [email , setemail] = useState("")
         const [password , setpassword] = useState("")
@@ -18,9 +18,16 @@ export default function Signin(){
 
         }
 
-        return <div className="flex flex-col h-screen justify-center items-center ">
-             <input className="w-[1000px] bg-slate-500  border-[2px]  border-black" placeholder="email" type="email" onChange={function(e){setemail(e.target.value)}}></input>
-        <input  className="w-[1000px] bg-slate-500  border-[2px]  border-black" placeholder="passoword" type="password" onChange={function(e){setpassword(e.target.value)}}></input>
-        <button className="w-[1000px] bg-slate-500  border-[2px]  border-black" onClick={handlesubmit} > SIGN IN</button>
+        return  <div className="w-screen bg-slate-800 h-screen gap-[2vh] font-serif   flex flex-col justify-center items-center">
+            <div className="text-slate-700 text-5xl">
+                <h1 >welcome to TASK.IO</h1>
+            </div>
+        <div className="flex flex-col h-[70vh] rounded-t-lg bg-slate-600  gap-4 justify-center items-center w-[100vw] lg:w-[50vw] border-black border-[2px] ">
+            <input className=" placeholder:text-center placeholder:text-[2vhvh] border-[2px] h-[7vh] bg-slate-400  placeholder:text-slate-900 focus:border-[4px] rounded-lg  border-slate-700 text-slate-700" placeholder="EMAIL" type="email" onChange={function(e){setemail(e.target.value)}}></input>
+            <input  className=" placeholder:text-center border-[2px] h-[7vh] rounded-lg bg-slate-400 focus:border-[4px] border-slate-800 placeholder:text-slate-900" placeholder="PASSWORD" type="password" onChange={function(e){setpassword(e.target.value)}}></input>
+        <button className=" border-[2px] h-[5vh] rounded-lg text-slate-800 w-[20vw] border-black text-nowrap hover:bg-slate-400" onClick={handlesubmit} > SIGN IN</button>
+        <h1>don't have an account ? <Link to="/signup" className="font-bold">SIGN UP</Link></h1>
         </div>
+        </div>
+
 }
